aee4f1b9c41a668d0fb6d822c815dc4b31f499c4
Jan witczak <j.witczak@onet.pl>
2021-12-06 22:16:54 +0100

    Added option to keep natural focus type for tribal characters.

--- Source/IdeologicalAnima/NaturalFocusPatch.cs
+++ Source/IdeologicalAnima/NaturalFocusPatch.cs
@@ -1,5 +1,6 @@
 ﻿using HarmonyLib;
 using System.Reflection;
+using UnityEngine;
 using Verse;
 
 namespace RimWorld
@@ -19,22 +20,54 @@ namespace RimWorld
 		[HarmonyPatch(typeof(MeditationFocusDef), "CanPawnUse")]
 		class NaturalFocusPatch
 		{
+			private static PreceptDef Trees_Precept;
 			static NaturalFocusPatch()
 			{
 				DefDatabase<PreceptDef>.GetNamed("Trees_Desired").description += "\n\n<color=#E6E64C>Enables meditation focus types:</color>\n - Natural";
+				Trees_Precept = DefDatabase<PreceptDef>.GetNamed("Trees_Desired");
 			}
 			static void Postfix(ref bool __result, string ___defName, Pawn p)
 			{
-				if (___defName == "Natural")
+				if (___defName == "Natural" && p.Ideo != null)
 				{
-					if (p.Ideo != null)
-					{
-						if (p.Ideo.HasPrecept(DefDatabase<PreceptDef>.GetNamed("Trees_Desired"))) __result = true;
-						else __result = false;
-					}
-					else __result = false;
+					if (p.Ideo.HasPrecept(Trees_Precept)) __result = true;
+					else if (!IdeologicalAnimaMod.Settings.InateTribalNaturalFocus) __result = false;
 				}
 			}
 		}
+
+		public class IdeologicalAnimaSettings : ModSettings
+		{
+			public bool InateTribalNaturalFocus = false;
+			public override void ExposeData()
+			{
+				Scribe_Values.Look(ref InateTribalNaturalFocus, "InateTribalFocus", defaultValue: false); ;
+				base.ExposeData();
+			}
+		}
+
+		[StaticConstructorOnStartup]
+		public class IdeologicalAnimaMod : Verse.Mod
+		{
+			public static IdeologicalAnimaSettings Settings;
+			public IdeologicalAnimaMod(ModContentPack content) : base(content)
+			{
+				Settings = GetSettings<IdeologicalAnimaSettings>();
+			}
+
+			public override void DoSettingsWindowContents(Rect inRect)
+			{
+				Listing_Standard listingStandard = new Listing_Standard();
+				listingStandard.Begin(inRect);
+				listingStandard.CheckboxLabeled("Inate natural focus type for characters with tribal upbringing.", ref Settings.InateTribalNaturalFocus);
+				listingStandard.End();
+				base.DoSettingsWindowContents(inRect);
+			}
+			public override string SettingsCategory()
+			{
+				return "Ideological Anima";
+			}
+		}
+
 	}
 }
