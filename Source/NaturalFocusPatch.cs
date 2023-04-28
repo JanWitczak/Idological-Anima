f465eb95a49481ac5b2cc15f41e0683fae917386
Jan witczak <Jan_Witczak@outlook.com>
2023-04-29 00:08:53 +0200

    Refactoring and Redesign.

--- Source/NaturalFocusPatch.cs
+++ Source/NaturalFocusPatch.cs
@@ -17,62 +17,47 @@ namespace IdeologicalAnima
 		}
 
 	}
-	[HarmonyPatch(typeof(MeditationFocusDef), "CanPawnUse")]
+	[HarmonyPatch(typeof(MeditationFocusTypeAvailabilityCache), "PawnCanUseInt")]
 	class NaturalFocusPatch
 	{
-		private static PreceptDef Trees_Precept;
 		static NaturalFocusPatch()
 		{
-			DefDatabase<PreceptDef>.GetNamed("Trees_Desired").description += "\n\n<color=#E6E64C>Enables meditation focus types:</color>\n - Natural";
-			Trees_Precept = DefDatabase<PreceptDef>.GetNamed("Trees_Desired");
+			TreesPreceptDefOf.Trees_Desired.description += "\n\n<color=#E6E64C>Enables meditation focus types:</color>\n - Natural";
 		}
-		static void Postfix(ref bool __result, string ___defName, Pawn p)
+		static void Postfix(ref bool __result, Pawn p, MeditationFocusDef type)
 		{
-			if (p.Ideo != null)
+			if (p.Ideo != null && !__result && type == MeditationFocusDefOf.Natural)
 			{
-				if (___defName == "Natural")
-				{
-					if (p.Ideo.HasPrecept(Trees_Precept)) __result = true;
-					else if (!IdeologicalAnimaMod.Settings.InateTribalNaturalFocus) __result = false;
-				}
-				else if (___defName == "Artistic")
-				{
-					if (!p.Ideo.HasPrecept(Trees_Precept) && !IdeologicalAnimaMod.Settings.InateTribalNaturalFocus) __result = true;
-				}
+				if(p.Ideo.HasPrecept(TreesPreceptDefOf.Trees_Desired)) __result = true;
 			}
 		}
 	}
-
-	public class IdeologicalAnimaSettings : ModSettings
+	[HarmonyPatch(typeof(Pawn_IdeoTracker), "SetIdeo")]
+	class ChangedIdeoPatch
 	{
-		public bool InateTribalNaturalFocus = false;
-		public override void ExposeData()
+		static void Postfix(Pawn_IdeoTracker __instance)
 		{
-			Scribe_Values.Look(ref InateTribalNaturalFocus, "InateTribalFocus", defaultValue: false); ;
-			base.ExposeData();
+			MeditationFocusTypeAvailabilityCache.ClearFor((Pawn)typeof(Pawn_IdeoTracker).GetField("pawn", BindingFlags.NonPublic | BindingFlags.Instance).GetValue(__instance));
 		}
 	}
-
-	public class IdeologicalAnimaMod : Verse.Mod
+	[HarmonyPatch(typeof(Ideo),"RecachePrecepts")]
+	class RecachePreceptsPatch
 	{
-		public static IdeologicalAnimaSettings Settings;
-		public IdeologicalAnimaMod(ModContentPack content) : base(content)
+		static void Postfix(Ideo __instance)
 		{
-			Settings = GetSettings<IdeologicalAnimaSettings>();
-		}
-
-		public override void DoSettingsWindowContents(Rect inRect)
-		{
-			Listing_Standard listingStandard = new Listing_Standard();
-			listingStandard.Begin(inRect);
-			listingStandard.CheckboxLabeled("Inate natural focus type for characters with tribal upbringing.", ref Settings.InateTribalNaturalFocus);
-			listingStandard.End();
-			base.DoSettingsWindowContents(inRect);
-		}
-		public override string SettingsCategory()
-		{
-			return "Ideological Anima";
+			foreach(Pawn pawn in PawnsFinder.AllMapsCaravansAndTravelingTransportPods_Alive)
+			{
+				if(pawn.Ideo == __instance) MeditationFocusTypeAvailabilityCache.ClearFor(pawn);
+			}
 		}
 	}
-
+	[DefOf]
+	static class TreesPreceptDefOf
+	{
+		public static PreceptDef Trees_Desired;
+		static TreesPreceptDefOf()
+		{
+			DefOfHelper.EnsureInitializedInCtor(typeof(TreesPreceptDefOf));
+		}
+	}
 }
